961dfa453fe57f52a677aabd3f92f0b0e7b753ed
Marcelo Ribeiro de Albuquerque <marcelo.albuquerque@xpi.com.br>
2021-12-14 23:52:33 -0300

    fix: ng lint fixes

--- angular/src/app/containers/order/order-list/order-list.component.spec.ts
+++ angular/src/app/containers/order/order-list/order-list.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { OrderListComponent } from './order-list.component';
+import { OrderListComponent } from './order-list.component'
 
 describe('OrderListComponent', () => {
-  let component: OrderListComponent;
-  let fixture: ComponentFixture<OrderListComponent>;
+  let component: OrderListComponent
+  let fixture: ComponentFixture<OrderListComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ OrderListComponent ]
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(OrderListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(OrderListComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
